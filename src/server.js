aad438304f3e12ffa7d2d056b3719c1b621b898d
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-16 09:39:33 +0000

    [BE][Modify] - Implement changes on server.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,18 @@
-const { createServer } = require("node:http");
-const { pool } = require("./db_conn");
+const express = require("express");
+const { sequelize } = require("./database/db_conn");
+const commonConstants = require("./common/constants");
+const app = express();
 
-async function CreatePerson(lastName, firstName, age) {
-  const res = await pool.query(
-      "INSERT INTO persons (lastName, firstName, age) VALUES ($1, $2, $3)",
-      [lastName, firstName, age]
-    );
-}
+const PORT = process.env.PORT
 
-// CreatePerson("ottsupi", "dev", 24)
+sequelize.authenticate()
+  .then(()=> {
+    console.log(commonConstants.DATABASE_CONNECTION.SUCCESS);
+  })
+  .catch((error) => {
+    console.log(commonConstants.DATABASE_CONNECTION.FAILED + error)
+  })
 
-const hostname = "0.0.0.0";
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running in the container at: ${hostname}:${port}`);
-  console.log(`Accessible from the host machine at: http://localhost:${port}/`);
-});
+app.listen(PORT, () => console.log(
+  commonConstants.SERVCER_CONNECTION.SUCCESS + PORT
+))
