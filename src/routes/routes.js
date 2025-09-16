e94f2a1b05d56d00899994d9583542681db92024
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-16 09:38:23 +0000

    [BE][Feature] - Added routes

---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+// const {} = require("")
+
+// router.route("/").post()
+// router.route("/:id").get()
+// router.route("/:id").delete();
+// router.route("/:id").put();
+// router.route("/").get();
+
+module.exports = router;
