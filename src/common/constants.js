c53181b416f74417e703109fef3d577e2e83323d
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-16 09:37:56 +0000

    [BE][Feature] - Added constants variables

---
+++ src/common/constants.js
@@ -0,0 +1,11 @@
+var commonConstants = {
+    DATABASE_CONNECTION: {
+        SUCCESS: "Database connection is successfully established.",
+        FAILED: "Unable to connect to the database."
+    },
+    SERVCER_CONNECTION: {
+        SUCCESS: "SERVER IS RUNNING on PORT"
+    },
+}
+
+module.exports = commonConstants
