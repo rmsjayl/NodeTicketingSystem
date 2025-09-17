3b3373c352e3544afbd1017ba17fb31d88f82c1d
ottsupi <ott.regisorate@gmail.com>
2025-09-17 11:44:23 +0800

    [ADHOC][Feature] - Added colors to command prompt

--- .devcontainer/bashrc.template.sh
+++ .devcontainer/bashrc.template.sh
@@ -12,3 +12,10 @@ HISTCONTROL=ignoreboth
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
+
+# Prompt with colored text
+if [[ ${EUID} == 0 ]] ; then
+    PS1='\[\033[38;2;221;75;57m\]\$ \[\033[38;2;0;135;175m\]\h \[\033[38;2;83;85;85m\]\w \[\033[0m\]'
+else
+    PS1='\[\033[38;2;105;121;16m\]\$ \[\033[38;2;0;135;175m\]\u@\h \[\033[38;2;83;85;85m\]\w \[\033[0m\]'
+fi
