81d3294503781bf2e42c4c0733123badffcc3ce5
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-17 07:26:24 +0000

    [BE][Feature] - Added auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,19 @@
+exports.register = async (req, res) => {
+    res.send("Register Route is working...");
+};
+
+exports.login = async (req, res) => {
+    res.send("Login route is working...");
+};
+
+exports.resetPassword = async (req, res) => {
+    res.send("Reset password route is working ...");
+};
+
+exports.updatePassword = async (req, res) => {
+    res.send("Update password route is working ...");
+};
+
+exports.forgotPassword = async (req, res) => {
+    res.send("Forgot password route is working ...");
+};
