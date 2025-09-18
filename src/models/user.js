3953243b7822a762115103c33ae6c0cafb7fdd37
ottsupi <ott.regisorate@gmail.com>
2025-09-18 08:03:12 +0000

    [BE][Modify] - Implement account verification
    - Also implement User.sync logging

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,5 @@
+const commonConstants = require("../common/constants");
+const commonHelpers = require("../common/helpers");
 const sequelize = require("../database/db_conn");
 const { DataTypes } = require("sequelize");
 
@@ -27,8 +29,36 @@ const User = sequelize.define("User", {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    isVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    accountVerficationToken: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    accountVerificationExpiry: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    }
 }, {
     timestamps: true,
 });
+
+User.afterCreate(async (user) => {
+    const token = commonHelpers.generateRandomToken();
+    user.accountVerficationToken = token;
+    user.accountVerificationExpiry = Date.now() + 30 * 60 * 1000;
+    console.log("After create hook")
+    commonHelpers.sendVerificationEmail(user.email, token)
+});
+
+User.sync({ alter: true })
+  .then(() => {
+    console.log(commonConstants.DATABASE_TABLES.USER + commonConstants.DATABASE_TABLE_CREATION.SUCCESS);
+  })
+  .catch((error) => {
+    console.error(`${commonConstants.DATABASE_TABLES.USER} ${commonConstants.DATABASE_CONNECTION.ERROR} ${error}`);
+  });
 
 module.exports = User
