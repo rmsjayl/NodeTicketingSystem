4149d76d08f546a89d2eb5c1e078bd1e150a4488
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-18 02:56:49 +0000

    [BE][Feature] - Added Get and Create method for users. Also added its corresponding route.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,69 @@
+const commonConstants = require("../common/constants")
+const User = require("../models/user")
+
 exports.getUsers = async (req, res) => {
-    res.send("GFet users endpoint...");
+    try {
+        const page = parseInt(req.query.page) || commonConstants.PAGINATION.PAGE;
+        const limit = parseInt(req.query.limit) || commonConstants.PAGINATION.LIMIT;
+        const offset = (page - 1) * limit;
+
+        const { count, rows} = await User.findAndCountAll({
+            order: [["createdAt", "DESC"]],
+            limit,
+            offset
+        });
+
+        const totalPage = Math.ceil(count / limit);
+
+        return res.status(commonConstants.STATUS_CODE.OK).json({
+            success: true,
+            message: commonConstants.USER.RETRIEVE.SUCCESS,
+            totalRecords: count,
+            pagination: {
+                page: `${page} out of ${totalPage}`,
+                limit: limit,
+            },
+            users: rows,
+        });
+
+    } catch (error) {
+        return res.status(commonConstants.STATUS_CODE.BAD_REQUEST).json({
+            success: false,
+            message: commonConstants.USER.RETRIEVE.FAILED + error.message
+        })
+    }
+}
+
+exports.createUser = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, roles } = req.body;
+
+        console.log(req.body);
+
+        const user = await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            roles: roles
+        });
+
+        const userResponse = {
+            id: user.id,
+            firstName: user.firstName,
+            email: user.email,
+            role: user.roles
+        }
+
+        res.status(commonConstants.STATUS_CODE.CREATED).json({
+            success: true,
+            message: commonConstants.USER.CREATE.SUCCESS,
+            user: userResponse
+        })
+    } catch (error) {
+        return res.status(commonConstants.STATUS_CODE.BAD_REQUEST).json({
+            success: false,
+            message: commonConstants.USER.CREATE.FAILED + error.message
+        });
+    }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsers } = require("../controllers/userController");
+const { getUsers, createUser } = require("../controllers/userController");
 
 router.route("/getUsers").get(getUsers)
+router.route("/create").post(createUser)
 
 module.exports = router;
