e7bc5cbc595013429a732356dadacc1fd327cfb0
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-18 03:20:53 +0000

    [BE][Fix] - Migrate to register route.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,38 @@
+const commonHelpers = require("../common/helpers");
+const User = require("../models/user");
+
 exports.register = async (req, res) => {
-    res.send("Register Route is working...");
+    try {
+        const { firstName, lastName, email, password, roles } = req.body;
+
+        console.log(req.body);
+
+        const user = await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: commonHelpers.passwordHasher(password),
+            roles: roles
+        });
+
+        const userResponse = {
+            id: user.id,
+            firstName: user.firstName,
+            email: user.email,
+            role: user.roles
+        }
+
+        res.status(commonConstants.STATUS_CODE.CREATED).json({
+            success: true,
+            message: commonConstants.USER.CREATE.SUCCESS,
+            user: userResponse
+        })
+    } catch (error) {
+        return res.status(commonConstants.STATUS_CODE.BAD_REQUEST).json({
+            success: false,
+            message: commonConstants.USER.CREATE.FAILED + error.message
+        });
+    }
 };
 
 exports.login = async (req, res) => {
