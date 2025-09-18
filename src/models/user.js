78f54bba64f258a503cb16fbe909cb7ea81a9c01
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-18 01:44:58 +0000

    [BE][Feature] - Implement user entity

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const sequelize = require("../database/db_conn");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    roles: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = User
