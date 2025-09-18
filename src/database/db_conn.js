01f46c163eeb6088c953a518ce16466784e4a3bc
rmsjayl <jaylord.ramos016@gmail.com>
2025-09-18 01:44:18 +0000

    [ADHOC][Modify] - For security purposes

--- src/database/db_conn.js
+++ src/database/db_conn.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize')
 
 const sequelize  = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER,process.env.POSTGRES_PASSWORD, {
-  host: 'database', // Service name of the database in `compose.dev.yaml`
+  host: process.env.DB_HOST, // Service name of the database in `compose.dev.yaml`
   dialect: process.env.DB_DIALECT
 })
 
-module.exports = { sequelize };
+module.exports = sequelize;
