b6a68f7ac2b5f564382a1ab8e3a87b3548fc3c09
Christian John Roa <roacj.cr@gmail.com>
2025-09-22 14:49:33 +0800

    Create file for centralized relationship declaration

---
+++ src/database/index.js
@@ -0,0 +1,59 @@
+const sequelize = require('./db_conn');
+const commonConstants = require('../common/constants');
+
+// Import models
+const User = require('../models/user');
+const Category = require('../models/category');
+const Ticket = require('../models/ticket');
+
+
+async function syncModels() {
+  
+  await User.sync({ alter: true })
+    .then(() => {
+      console.log(commonConstants.DATABASE_TABLES.USER + commonConstants.DATABASE_TABLE_CREATION.SUCCESS);
+    })
+    .catch((error) => {
+      console.error(`${commonConstants.DATABASE_TABLES.USER} ${commonConstants.DATABASE_CONNECTION.ERROR} ${error.message}`);
+    });
+
+  await Ticket.sync({ alter: true })
+    .then(() => {
+      console.log(commonConstants.DATABASE_TABLES.TICKET + commonConstants.DATABASE_TABLE_CREATION.SUCCESS);
+    })
+    .catch((error) => {
+      console.error(`${commonConstants.DATABASE_TABLES.TICKET} ${commonConstants.DATABASE_CONNECTION.ERROR} ${error.message}`);
+    });
+
+  await Category.sync({ alter: true })
+    .then(() => {
+      console.log(commonConstants.DATABASE_TABLES.CATEGORY + commonConstants.DATABASE_TABLE_CREATION.SUCCESS);
+    })
+    .catch((error) => {
+      console.error(`${commonConstants.DATABASE_TABLES.CATEGORY} ${commonConstants.DATABASE_CONNECTION.ERROR} ${error.message}`);
+    });
+}
+
+syncModels();
+
+// --- Define Associations ---
+
+// User <-> Ticket (One-to-Many)
+User.hasMany(Ticket, { foreignKey: 'userId', as: 'tickets' });
+Ticket.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+// Category <-> Ticket (One-to-Many)
+Category.hasMany(Ticket, { foreignKey: 'categoryId', as: 'tickets' });
+Ticket.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
+
+
+// Export all models and the sequelize instance
+const db = {
+  sequelize,
+  User,
+  Category,
+  Ticket,
+};
+
+module.exports = db;
+
